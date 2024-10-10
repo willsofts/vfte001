9b7117fb02a671ab6fe122e517baea2180780f5a
tassun <32536248+tassun@users.noreply.github.com>
2024-10-10 16:49:46 +0700

    initial project

---
+++ src/AppVfte001.vue
@@ -0,0 +1,158 @@
+<!-- App.vue -->
+<template>
+  <div id="fswaitlayer" class="fa fa-spinner fa-spin"></div>
+  <div class="pt-page pt-page-current pt-page-controller search-pager">
+    <PageHeader ref="pageHeader" :labels="labels" pid="vfte001" version="1.0.0" showLanguage="true" @language-changed="changeLanguage" :multiLanguages="multiLanguages" />
+    <SearchForm ref="searchForm" :labels="labels" :dataCategory="dataCategory" @data-select="dataSelected" @data-insert="dataInsert" />
+  </div>
+  <teleport to="#modaldialog">
+    <EntryForm ref="entryForm" :labels="labels" :dataCategory="dataCategory" @data-saved="dataSaved" @data-updated="dataUpdated" @data-deleted="dataDeleted" />
+  </teleport>
+</template>
+<script>
+import { ref } from 'vue';
+import $ from "jquery";
+import { PageHeader } from '@willsofts/will-control';
+import SearchForm from '@/components/SearchForm.vue';
+import EntryForm from '@/components/EntryForm.vue';
+import { getLabelModel } from "@willsofts/will-app";
+import { DEFAULT_CONTENT_TYPE, getDefaultLanguage, setDefaultLanguage, getApiUrl, getMultiLanguagesModel } from "@willsofts/will-app";
+import { startApplication, serializeParameters } from "@willsofts/will-app";
+
+export default {
+  components: {
+    PageHeader, SearchForm, EntryForm
+  },
+  setup() {
+    const dataChunk = {};
+    const dataCategory = {
+      tprod: [],
+      tkappstype: [],
+      tkprogtype: [],
+      tkprogsystem: [],
+    };
+    const multiLanguages = ref(getMultiLanguagesModel());
+    let labels = ref(getLabelModel());
+    let alreadyLoading = ref(false);
+    return { multiLanguages, labels, dataCategory, dataChunk, alreadyLoading };
+  },
+  mounted() {
+    console.log("App: mounted ...");
+    this.$nextTick(() => {
+      //ensure ui completed then invoke startApplication 
+      startApplication("vfte001",(data) => {
+        this.multiLanguages = getMultiLanguagesModel();
+        this.messagingHandler(data);
+        this.loadDataCategories(!this.alreadyLoading,() => {
+          this.$refs.pageHeader.changeLanguage(getDefaultLanguage());
+        });
+      });
+      //try to find out parameters from url
+      const searchParams = new URLSearchParams(window.location.href);
+      console.log("param: authtoken=",searchParams.get("authtoken"),", language=",searchParams.get("language"));      
+    });
+  },
+  methods: {
+    messagingHandler(data) {
+      console.log("messagingHandler: data",data); 
+    },
+    changeLanguage(lang) {
+      setDefaultLanguage(lang);
+      let labelModel = getLabelModel(lang);
+      this.labels = labelModel;
+      this.resetDataCategories(lang);
+    },
+    loadDataCategories(loading,callback) {
+      console.log("loadDataCategories: loading",loading);
+      if(!loading) return;
+      let jsondata = {names: ["tprod", "tkappstype", "tkprogtype", "tkprogsystem"]};
+      let formdata = serializeParameters(jsondata);
+      $.ajax({
+        url: getApiUrl()+"/api/category/lists",
+        data: formdata.jsondata,
+        headers : formdata.headers,
+        type: "POST",
+        dataType: "json",
+        contentType: DEFAULT_CONTENT_TYPE,
+        error : function(transport,status,errorThrown) {
+          console.error("loadDataCategories: error: status",status,"errorThrown",errorThrown);
+        },
+        success: (data) => {
+          this.alreadyLoading = true;
+          console.log("loadDataCategories: success",data);
+          if(data.body) {
+            for(let item of data.body) {
+              if(item.category && item.resultset && item.resultset.rows) {
+                this.dataChunk[item.category] = item.resultset.rows;
+              }
+            }
+            console.log("data chunk",this.dataChunk);
+            this.resetDataCategories();
+            if(callback) callback();
+          }
+        }
+      });	
+    },
+    resetDataCategories(lang) {
+      if(!lang) lang = getDefaultLanguage();
+      if(!lang || lang.trim().length==0) lang = "EN";
+      let tprod;
+      let tkappstype;
+      let tkprogtype;
+      let tkprogsystem;
+      let tk_categories = this.dataChunk["tprod"];
+      if(tk_categories) {
+        tprod = tk_categories.map((item) => { return { id: item.product, text: "TH"==lang?item.nameth:item.nameen } });
+      }
+      tk_categories = this.dataChunk["tkappstype"];
+      if(tk_categories) {
+        tkappstype = tk_categories.map((item) => { return { id: item.typeid, text: "TH"==lang?item.nameth:item.nameen } });
+      }
+      tk_categories = this.dataChunk["tkprogtype"];
+      if(tk_categories) {
+        tkprogtype = tk_categories.map((item) => { return { id: item.typeid, text: "TH"==lang?item.nameth:item.nameen } });
+      }
+      tk_categories = this.dataChunk["tkprogsystem"];
+      if(tk_categories) {
+        tkprogsystem = tk_categories.map((item) => { return { id: item.typeid, text: "TH"==lang?item.nameth:item.nameen } });
+      }
+      if(tprod) this.dataCategory.tprod = tprod;
+      if(tkappstype) this.dataCategory.tkappstype = tkappstype;
+      if(tkprogtype) this.dataCategory.tkprogtype = tkprogtype;
+      if(tkprogsystem) this.dataCategory.tkprogsystem = tkprogsystem;
+    },
+    dataSelected(item,action) {
+      //listen action from search form
+      console.log("App: dataSelected",item,"action",action);
+      if("edit"==action) {
+        this.$refs.entryForm.retrieveRecord({programid: item.programid});
+      } else if("delete"==action) {
+        this.$refs.entryForm.startDeleteRecord({programid: item.programid});
+      }
+    },
+    dataInsert(filters) {
+      //listen action from search form
+      console.log("App: record insert",filters);
+      this.$refs.entryForm.startInsertRecord();
+    },
+    dataSaved(data,response) {
+      //listen action from entry form when after saved
+      console.log("App: record saved");
+      console.log("data",data,"response",response);
+      this.$refs.searchForm.search();
+    },
+    dataUpdated(data,response) {
+      //listen action from entry form when after updated
+      console.log("App: record updated");
+      console.log("data",data,"response",response);
+      this.$refs.searchForm.search();
+    },
+    dataDeleted(data,response) {
+      //listen action from entry form when after deleted
+      console.log("App: record deleted");
+      console.log("data",data,"response",response);
+      this.$refs.searchForm.search(true);
+    },
+  }
+};
+</script>
---
+++ src/components/EntryForm.vue
@@ -0,0 +1,414 @@
+<template>
+  <DialogForm ref="dialogForm">
+    <template v-slot:header>
+      <h4 class="modal-title" v-if="insertMode">{{ labels.title_new }}</h4>
+      <h4 class="modal-title" v-if="updateMode">{{ labels.title_edit }}</h4>
+    </template>
+    <template v-slot:default>
+        <div class="row row-height">
+          <div class="col-height col-md-5">
+            <label for="programid" class="control-label">{{ labels.programid_label }}</label>
+            <div class="input-group has-validation" :class="{'has-error': v$.programid.$error}">
+              <input ref="programid" type="text" v-model="localData.programid" id="programid" class="form-control input-md" maxlength="20" :disabled="disabledKeyField" />
+              <label class="required">*</label>
+            </div>
+            <span v-if="v$.programid.$error" class="has-error">{{ v$.programid.$errors[0].$message }}</span>
+          </div>
+        </div>
+        <div class="row row-height">
+          <div class="col-height col-md-10">
+            <label for="progname" class="control-label">{{ labels.progname_label }}</label>
+            <div class="input-group has-validation" :class="{'has-error': v$.progname.$error}">
+              <input type="text" ref="progname" v-model="localData.progname" id="progname" name="progname" class="form-control input-md" maxlength="50" /> 
+              <label class="required">*</label>
+            </div>
+            <span v-if="v$.progname.$error" class="has-error">{{ v$.progname.$errors[0].$message }}</span>
+          </div>
+        </div>
+        <div class="row row-height">
+          <div class="col-height col-md-10">
+            <label for="prognameth" class="control-label">{{ labels.prognameth_label }}</label>
+            <div class="input-group has-validation" :class="{'has-error': v$.prognameth.$error}">
+              <input tyep="text" ref="prognameth" v-model="localData.prognameth" id="prognameth" name="prognameth" class="form-control input-md" maxlength="50" />
+              <label class="required">*</label> 
+            </div>
+            <span v-if="v$.prognameth.$error" class="has-error">{{ v$.prognameth.$errors[0].$message }}</span>
+          </div>
+        </div>
+        <div class="row row-height">
+          <div class="col-height col-md-5">
+            <label for="product" class="control-label">{{ labels.product_label }}</label>
+            <div class="input-group has-validation" :class="{'has-error': v$.product.$error}">
+              <select ref="product" v-model="localData.product" class="form-control input-md">
+                <option value=""></option>
+                <option v-for="item in dataCategory.tprod" :key="item.id" :value="item.id">{{item.text}}</option>
+              </select>
+              <label class="required">*</label>
+            </div>
+            <span v-if="v$.product.$error" class="has-error">{{ v$.product.$errors[0].$message }}</span>
+          </div>
+          <div class="col-height col-md-5">
+            <label for="progtype" class="control-label">{{ labels.progtype_label }}</label>
+            <div class="input-group has-validation" :class="{'has-error': v$.progtype.$error}">
+              <select ref="progtype" v-model="localData.progtype" class="form-control input-md">
+                <option v-for="item in dataCategory.tkprogtype" :key="item.id" :value="item.id">{{item.text}}</option>
+              </select>
+              <label class="required">*</label>
+            </div>
+            <span v-if="v$.progtype.$error" class="has-error">{{ v$.progtype.$errors[0].$message }}</span>
+          </div>
+        </div>
+        <div class="row row-height">
+          <div class="col-height col-md-5">
+            <label for="progsystem" class="control-label">{{ labels.progsystem_label }}</label>
+            <div class="input-group has-validation" :class="{'has-error': v$.progsystem.$error}">
+              <select ref="progsystem" v-model="localData.progsystem" class="form-control input-md">
+                <option v-for="item in dataCategory.tkprogsystem" :key="item.id" :value="item.id">{{item.text}}</option>
+              </select>
+              <label class="required">*</label>
+            </div>
+            <span v-if="v$.progsystem.$error" class="has-error">{{ v$.progsystem.$errors[0].$message }}</span>
+          </div>
+          <div class="col-height col-md-5">
+            <label for="appstype" class="control-label">{{ labels.appstype_label }}</label>
+            <div class="input-group has-validation" :class="{'has-error': v$.appstype.$error}">
+              <select ref="appstype" v-model="localData.appstype" class="form-control input-md">
+                <option v-for="item in dataCategory.tkappstype" :key="item.id" :value="item.id">{{item.text}}</option>
+              </select>
+              <label class="required">*</label>
+            </div>
+            <span v-if="v$.appstype.$error" class="has-error">{{ v$.appstype.$errors[0].$message }}</span>
+          </div>
+        </div>
+        <div class="row row-height">
+          <div class="col-height col-md-10">
+            <label for="shortname" class="control-label">{{ labels.shortname_label }}</label>
+            <div class="input-group has-validation" :class="{'has-error': v$.shortname.$error}">
+              <input ref="shortname" type="text" v-model="localData.shortname" id="shortname" class="form-control input-md" maxlength="50" />
+              <label class="required">*</label>
+            </div>
+            <span v-if="v$.shortname.$error" class="has-error">{{ v$.shortname.$errors[0].$message }}</span>
+          </div>
+        </div>
+        <div class="row row-height">
+          <div class="col-height col-md-10">
+            <label for="shortnameth" class="control-label">{{ labels.shortnameth_label }}</label>
+            <div class="input-group has-validation" :class="{'has-error': v$.shortnameth.$error}">
+              <input ref="shortnameth" type="text" v-model="localData.shortnameth" id="shortnameth" class="form-control input-md" maxlength="50" />
+              <label class="required">*</label>
+            </div>
+            <span v-if="v$.shortnameth.$error" class="has-error">{{ v$.shortnameth.$errors[0].$message }}</span>
+          </div>
+        </div>
+        <div class="row row-height">
+          <div class="col-height col-md-10">
+            <label for="description" class="control-label">{{ labels.description_label }}</label>
+            <input ref="description" type="text" v-model="localData.description" id="description" class="form-control input-md" maxlength="100" />
+          </div>
+        </div>
+        <div class="row row-height">
+          <div class="col-height col-md-10">
+            <label for="parameters" class="control-label">{{ labels.parameters_label }}</label>
+            <input ref="parameters" type="text" v-model="localData.parameters" id="parameters" class="form-control input-md" maxlength="50" />
+          </div>
+        </div>
+        <div class="row row-height">
+          <div class="col-height col-md-10">
+            <label for="progpath" class="control-label">{{ labels.progpath_label }}</label>
+            <input ref="progpath" type="text" v-model="localData.progpath" id="progpath" class="form-control input-md" maxlength="100" />
+          </div>
+        </div>
+        <div class="row row-height">
+          <div class="col-height col-md-6">
+            <label class="control-label">{{ labels.iconstyle_label }}</label>
+						<div ref="iconstyleswitcher" id="iconstyleswitcher"></div>							
+          </div>
+        </div>
+        <div class="row row-height">
+          <div class="col-md-5 col-height" id="iconfilelayer">
+            <label class="control-label">{{ labels.iconfile_label }}</label>
+          </div>
+        </div>
+        <div class="row row-height">
+          <div class="col-md-1"></div>
+          <div class="col-md-9 col-height">
+            <img id="iconfileimage" width="128px" height="128px" :src="getIconImage" alt="Program Image"/>
+          </div>
+        </div>
+        <input type="hidden" id="iconstyle" v-model="localData.iconstyle" />
+    </template>
+    <template v-slot:footer>
+      <button class="btn btn-dark btn-sm" @click="saveClick" v-if="insertMode"><em class="fa fa-save fa-btn-icon"></em>{{ labels.save_button }}</button>
+      <button class="btn btn-dark btn-sm" @click="updateClick" v-if="updateMode"><em class="fa fa-save fa-btn-icon"></em>{{ labels.update_button }}</button>
+      <button class="btn btn-dark btn-sm" data-dismiss="modal"><em class="fa fa-close fa-btn-icon"></em>{{ labels.cancel_button }}</button>
+    </template>
+  </DialogForm>
+</template>
+<script>
+import { ref, computed, watch } from 'vue';
+import { useVuelidate } from '@vuelidate/core';
+import { required, helpers } from '@vuelidate/validators';
+import $ from "jquery";
+import { DEFAULT_CONTENT_TYPE, getApiUrl, getImgUrl }  from '@willsofts/will-app';
+import { startWaiting, stopWaiting, submitFailure, detectErrorResponse }  from '@willsofts/will-app';
+import { confirmUpdate, confirmSave, confirmDelete, successbox, serializeParameters } from '@willsofts/will-app';
+import DialogForm from './DialogForm.vue';
+
+const defaultData = {
+  programid: '',
+  progname: "",
+  prognameth: "",
+  product: "",
+  progtype: "E",
+  progsystem: "F",
+  appstype: "W",
+  shortname: "",
+  shortnameth: "",
+  description: "",
+  parameters: "",
+  progpath: "",
+  iconfile: "",
+  iconstyle: "",
+};
+
+export default {
+  components: {
+    DialogForm
+  },
+  props: {
+    modes: Object,
+    labels: Object,
+    dataCategory: Object
+  },
+  emits: ["data-saved","data-updated","data-deleted"],
+  setup(props) {
+    const mode = ref({action: "new", ...props.modes});
+    const localData = ref({ ...defaultData }); 
+    const disabledKeyField = ref(false);
+    const reqalert = ref(props.labels.empty_alert);
+    const requiredMessage = () => {
+      return helpers.withMessage(reqalert, required);
+    }
+    const validateRules = computed(() => { 
+      return {
+        programid: { required: requiredMessage() },
+        progname: { required: requiredMessage() },
+        prognameth: { required: requiredMessage() },
+        product: { required: requiredMessage() },
+        progtype: { required: requiredMessage() },
+        progsystem: { required: requiredMessage() },
+        appstype: { required: requiredMessage() },
+        shortname: { required: requiredMessage() },
+        shortnameth: { required: requiredMessage() },
+      } 
+    });
+    const v$ = useVuelidate(validateRules, localData, { $lazy: true, $autoDirty: true });
+    return { mode, v$, localData, disabledKeyField, reqalert };
+  },
+  created() {
+    watch(this.$props, (newProps) => {      
+      this.reqalert = newProps.labels.empty_alert;
+    });
+  },
+  computed: {
+    insertMode() {
+      return this.mode.action=="insert" || this.mode.action=="new";
+    },
+    updateMode() {
+      return this.mode.action=="update" || this.mode.action=="edit";
+    },
+    getIconImage() {
+      return this.localData.iconfile && this.localData.iconfile.trim().length > 0 ? getImgUrl()+"/img/apps/"+this.localData.iconfile : getImgUrl()+"/img/apps/apps.png";
+    },
+  },
+  mounted() {
+    this.$nextTick(function () {
+      $("#modaldialog_layer").find(".modal-dialog").draggable();
+      $("#iconstyleswitcher").styleswitcher({$styleInput: $("#iconstyle"), width: 210, styleURL : getApiUrl()+"/api/style/category" });	
+    });
+  },
+  methods: {
+    reset(newData,newMode) {
+      if(newData) this.localData = {...newData};
+      if(newMode) this.mode = {...newMode};
+    },
+    submit() {      
+      this.$emit('update:formData', this.localData);
+    },
+    async saveClick() {
+      console.log("click: save");
+      console.log("localData",this.localData);
+      let valid = await this.validateForm();
+      if(!valid) return;
+      this.startSaveRecord();
+    },
+    async updateClick() {
+      console.log("click: update");
+      console.log("localData",this.localData);
+      let valid = await this.validateForm();
+      if(!valid) return;
+      this.startUpdateRecord();
+    },
+    async validateForm(focusing=true) {
+      const valid = await this.v$.$validate();
+      console.log("validate form: valid",valid);
+      console.log("error:",this.v$.$errors);
+      if(!valid) {
+        if(focusing) {
+          this.focusFirstError();
+        }
+        return false;
+      }
+      return true;
+    },
+    focusFirstError() {
+      if(this.v$.$errors && this.v$.$errors.length > 0) {
+        let input = this.v$.$errors[0].$property;
+        let el = this.$refs[input];
+        if(el) el.focus(); //if using ref
+        else $("#"+input).trigger("focus"); //if using id
+      }
+    },
+    showDialog() {
+      //$("#modaldialog_layer").modal("show");
+      $(this.$refs.dialogForm.$el).modal("show");
+    },  
+    hideDialog() {
+      //$("#modaldialog_layer").modal("hide");
+      $(this.$refs.dialogForm.$el).modal("hide");
+    },
+    resetRecord() {
+      //reset to default data 
+      this.reset(defaultData,{action:"insert"});
+      //reset validator
+      this.v$.$reset();
+      //enable key field
+      this.disabledKeyField = false;
+    },
+    startInsertRecord() {
+      this.resetRecord();
+      this.showDialog();
+    },
+    startSaveRecord() {
+      confirmSave(() => {
+        this.saveRecord(this.localData);
+      });
+    },
+    startUpdateRecord() {
+      confirmUpdate(() => { 
+        this.updateRecord(this.localData);
+      });
+    },
+    startDeleteRecord(dataKeys) {
+      confirmDelete(Object.values(dataKeys),() => {
+        this.deleteRecord(dataKeys);
+      });
+    },
+    saveRecord(dataRecord) {
+        let jsondata = {ajax: true};
+        let formdata = serializeParameters(jsondata,dataRecord);
+        startWaiting();
+        $.ajax({
+          url: getApiUrl()+"/api/sfte001/insert",
+          data: formdata.jsondata,
+          headers : formdata.headers,
+          type: "POST",
+          dataType: "json",
+          contentType: DEFAULT_CONTENT_TYPE,
+          error : function(transport,status,errorThrown) {
+            console.error("error: status",status,"errorThrown",errorThrown);
+            submitFailure(transport,status,errorThrown);
+          },
+          success: (data) => {
+            stopWaiting();
+            console.log("success",data);
+            if(detectErrorResponse(data)) return;
+            successbox(() => {
+              //reset data for new record insert
+              this.resetRecord();
+              this.$refs.programid.focus();
+            });
+            this.$emit('data-saved',dataRecord,data);
+          }
+      });
+    },
+    updateRecord(dataRecord) {
+        let jsondata = {ajax: true};
+        let formdata = serializeParameters(jsondata,dataRecord);
+        startWaiting();
+        $.ajax({
+          url: getApiUrl()+"/api/sfte001/update",
+          data: formdata.jsondata,
+          headers : formdata.headers,
+          type: "POST",
+          dataType: "json",
+          contentType: DEFAULT_CONTENT_TYPE,
+          error : function(transport,status,errorThrown) {
+            console.error("error: status",status,"errorThrown",errorThrown);
+            submitFailure(transport,status,errorThrown);
+          },
+          success: (data) => {
+            stopWaiting();
+            console.log("success",data);
+            if(detectErrorResponse(data)) return;
+            successbox(() => {
+              this.hideDialog();
+            });
+            this.$emit('data-updated',dataRecord,data);
+          }
+      });
+    },
+    retrieveRecord(dataKeys) {
+      let jsondata = {ajax: true};
+      let formdata = serializeParameters(jsondata,dataKeys);
+      startWaiting();
+      $.ajax({
+        url: getApiUrl()+"/api/sfte001/retrieve",
+        data: formdata.jsondata,
+        headers : formdata.headers,
+        type: "POST",
+        dataType: "json",
+        contentType: DEFAULT_CONTENT_TYPE,
+        error : function(transport,status,errorThrown) {
+          console.error("retrieveRecord: error: status",status,"errorThrown",errorThrown);
+          submitFailure(transport,status,errorThrown);
+        },
+        success: (data) => {
+          stopWaiting();
+          console.log("retrieveRecord: success",data);
+          if(data.body.dataset) {
+            this.reset(data.body.dataset,{action:"edit"});
+            this.v$.$reset();
+            this.disabledKeyField = true;
+            this.showDialog();
+          }
+        }
+      });	
+    },
+    deleteRecord(dataKeys) {
+      let jsondata = {ajax: true};
+      let formdata = serializeParameters(jsondata,dataKeys);
+      startWaiting();
+      $.ajax({
+        url: getApiUrl()+"/api/sfte001/remove",
+        data: formdata.jsondata,
+        headers : formdata.headers,
+        type: "POST",
+        dataType: "json",
+        contentType: DEFAULT_CONTENT_TYPE,
+        error : function(transport,status,errorThrown) {
+          console.error("deleteRecord: error: status",status,"errorThrown",errorThrown);
+          submitFailure(transport,status,errorThrown);
+        },
+        success: (data) => {
+          stopWaiting();
+          console.log("deleteRecord: success",data);
+          if(detectErrorResponse(data)) return;
+          successbox();
+          this.$emit('data-deleted',dataKeys,data);
+        }
+      });	
+    },
+  }
+};
+</script>
